73703798d90a06aa0b65bf47f113de38336ad676
David <d.ferri.2018@urjc.alumnos.es>
2019-02-15 18:06:38 +0100

    duck_duck

---
+++ practica_duck.py
@@ -0,0 +1,9 @@
+
+ def duck(j,n):
+     jugadors=["1","2","3 "]
+     nombre = duck(jugadores,2)
+     print(nombre)
+duck()
+
+print("hola jeje jaja juju")
+https://github.com/DavidFerri7/repositorio_david.git
